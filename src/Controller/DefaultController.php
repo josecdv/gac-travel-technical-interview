b02af708732ddb8346d72b89d2059188b2b02809
rick <martin.aranda.ricardo@gmail.com>
2022-03-22 10:09:00 +0100

    Primera pagina, base.html.twit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+   /**
+    * @Route("/")
+    */
+public function number(): Response
+    {
+        return $this->render('public/index.html.twig', [ ]);
+        return new Response ('<html><body> it is work </body></html>');
+
+    }
+}
