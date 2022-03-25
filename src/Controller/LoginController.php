dead630574bc86bb79454c520d54dd58d953f06d
rick <martin.aranda.ricardo@gmail.com>
2022-03-25 09:54:47 +0100

    Access Control login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Users;
+use App\Form\UsersType;
+use App\Repository\UsersRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class LoginController extends AbstractController
 {
@@ -19,6 +24,37 @@ class LoginController extends AbstractController
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
+        ]);
+    }
+    #[Route('/new', name: 'app_users_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, UsersRepository $usersRepository, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new Users();
+        $form = $this->createForm(UsersType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+            $plaintextPassword = $user->getPassword();
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hashedPassword);
+            $user->setActive(TRUE);
+            $user->setCreatedAt(new \DateTime());
+
+            //to-fix Unificar username y Uuid
+            $user->setUuid($user->getUsername());
+            $usersRepository->add($user);
+
+            return $this->redirectToRoute('app_users_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('users/registro.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
 }
--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-#[Route('/users')]
+#[Route('/stock/users')]
 class UsersController extends AbstractController
 {
     #[Route('/list', name: 'app_users_index', methods: ['GET'])]
@@ -19,38 +19,6 @@ class UsersController extends AbstractController
     {
         return $this->render('users/list.html.twig', [
             'users' => $usersRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_users_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, UsersRepository $usersRepository, UserPasswordHasherInterface $passwordHasher): Response
-    {
-        $user = new Users();
-        $form = $this->createForm(UsersType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $user = $form->getData();
-            $plaintextPassword = $user->getPassword();
-            $hashedPassword = $passwordHasher->hashPassword(
-                $user,
-                $plaintextPassword
-            );
-            $user->setPassword($hashedPassword);
-            $user->setActive(TRUE);
-            $user->setCreatedAt(new \DateTime());
-
-            //to-fix Unificar username y Uuid
-            $user->setUuid($user->getUsername());
-            $usersRepository->add($user);
-
-            return $this->redirectToRoute('app_users_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('users/registro.html.twig', [
-            'user' => $user,
-            'form' => $form,
         ]);
     }
 
