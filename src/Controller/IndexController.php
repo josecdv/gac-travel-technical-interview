36bf4ef1cccf76be9003c0d7827c1dd9d8486e3e
Daniel Pipicello <pipicello.daniel@gmail.com>
2023-11-04 19:59:29 +0100

    Login

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,12 +3,20 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class IndexController extends AbstractController
 {
-    #[Route('/')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('index.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 }
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass=UsersRepository::class)
+ * @ORM\Entity(repositoryClass=App\Repository\UsersRepository::class)
  */
 class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
